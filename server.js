2e7dfab6c75d279b608909c42994d1dd3b095bc2
Crhistian <cramirez@four51.com>
2022-06-29 12:31:31 -0500

    Add complete documentation including creation of supporting entities

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const port = process.env.PORT || 4451;
 const parseJwt = require("jwt-decode");
 const OrderCloud = require("ordercloud-javascript-sdk");
+ /**
+  * Please review these settings to ensure they match what you have in your marketplace
+  */
+const ORDERCLOUD_BASE_API_URL = "https://sandboxapi.ordercloud.io"
+const ORDERCLOUD_BUYER_ID = "buyer1";
+
 OrderCloud.Configuration.Set({
-  baseApiUrl: "https://sandboxapi.ordercloud.io",
+  baseApiUrl: ORDERCLOUD_BASE_API_URL,
 });
 const { v4: uuidv4 } = require("uuid");
-const BUYER_ID = "initBuyer";
 
 app.use(express.json());
 
@@ -15,6 +20,7 @@ app.use(express.json());
 // It is responsible for associating a user from the idp with a user from ordercloud and expects a username be returned
 app.post("/integration-events/createuser", (req, res) => {
   const body = req.body;
+  console.log(`Ordercloud access token`, body.OrderCloudAccessToken);
   OrderCloud.Tokens.SetAccessToken(body.OrderCloudAccessToken);
 
   // The claims (user details) from parsing the idp's ID token
@@ -23,7 +29,7 @@ app.post("/integration-events/createuser", (req, res) => {
 
   // We get an access token from the IDP which can be used to call out to the idp's api
   // in google's case we could pass along additional claims and upload something using google drive api for example
-  console.log(body.TokenResponse.access_token);
+  console.log(`IDP access token`, body.TokenResponse.access_token);
 
   // Here we're creating a new user and associating that new user with the idp login
   const newUser = {
@@ -34,16 +40,18 @@ app.post("/integration-events/createuser", (req, res) => {
     Active: true,
   };
 
+  console.log(`new user to create in buyerID ${ORDERCLOUD_BUYER_ID} \n ${JSON.stringify(newUser, null, 4)} `)
+
   // You can imagine doing something else here like assigning the user to a relevant group based on
   // the claims from the IDP - your imagination is the limit
   return (
-    OrderCloud.Users.Create(BUYER_ID, newUser)
+    OrderCloud.Users.Create(ORDERCLOUD_BUYER_ID, newUser)
 
       // The only thing OrderCloud cares about is getting the name of a user
       // here we are creating a brand new user but we could have just as easily returned
       // the username of an existing user
       .then((user) => {
-        console.log(`Created user with username ${user.Username}`);
+        console.log(`Success! Created user with username ${user.Username}`);
         return res
           .status(200)
           .send({ ErrorMessage: null, Username: newUser.Username });
@@ -55,6 +63,8 @@ app.post("/integration-events/createuser", (req, res) => {
 
         // We can tell the API to display errors to the user, helpful for debugging
         // any unhandled exceptions will be displayed as a raw value
+        console.log(`An error occurred while creating the user`);
+        console.log(message);
         return res.status(200).send({ ErrorMessage: message, Username: null });
       })
   );
